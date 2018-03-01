c1bc05e7f73ab27c78ea32f11be0378f7daa42b4
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-03-01 16:58:50 -0700

    Add default contacts and the ListContacts component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+
+const contacts = [
+  {
+    id: 'tyler',
+    name: 'Tyler McGinnis',
+    handle: '@tylermcginnis',
+    avatarURL: 'http://localhost:5001/tyler.jpg'
+  },
+  {
+    id: 'karen',
+    name: 'Karen Isgrigg',
+    handle: '@karen_isgrigg',
+    avatarURL: 'http://localhost:5001/karen.jpg'
+  },
+  {
+    id: 'richard',
+    name: 'Richard Kalehoff',
+    handle: '@richardkalehoff',
+    avatarURL: 'http://localhost:5001/richard.jpg'
+  },
+]
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={contacts} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App

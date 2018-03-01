f1fbeff5533d82e33b075573fff684a99a82b845
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-03-01 13:42:15 -0700

    Add ContactList component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContactList contacts={[
+          { name: 'Tyler '},
+          { name: 'Karen' },
+          { name: 'Richard '}
+        ]}/>
+        <ContactList contacts={[
+          { name: 'Amanda' },
+          { name: 'Mikenzi' },
+          { name: 'Ryan' }
+        ]}/>
       </div>
     );
   }

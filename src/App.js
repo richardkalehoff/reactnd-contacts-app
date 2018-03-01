7d19a3987df9a2fa28f8d3b60e2d5f231cf0cb4c
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-03-01 14:35:41 -0700

    Starting files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import React, { Component } from 'react';
 
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map((person) => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Tyler '},
-          { name: 'Karen' },
-          { name: 'Richard '}
-        ]}/>
-        <ContactList contacts={[
-          { name: 'Amanda' },
-          { name: 'Mikenzi' },
-          { name: 'Ryan' }
-        ]}/>
+      <div>
+        Hello World
       </div>
     );
   }

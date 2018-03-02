1671c5ca884e7f902ac9069d866079df34e42be1
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-03-01 20:41:23 -0700

    Filter and sort the contacts that are searched for

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -15,20 +15,28 @@ class ListContacts extends Component {
     }))
   }
   render() {
+    const { query } = this.state
+    const { contacts, onDeleteContact } = this.props
+
+    const showingContacts = query === ''
+      ? contacts
+      : contacts.filter((c) => (
+          c.name.toLowerCase().includes(query.toLowerCase())
+        ))
+
     return (
       <div className='list-contacts'>
-        {JSON.stringify(this.state)}
         <div className='list-contacts-top'>
           <input
             className='search-contacts'
             type='text'
             placeholder='Search Contacts'
-            value={this.state.query}
+            value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
         <ol className='contact-list'>
-          {this.props.contacts.map((contact) => (
+          {showingContacts.map((contact) => (
             <li key={contact.id} className='contact-list-item'>
               <div
                 className='contact-avatar'
@@ -41,7 +49,7 @@ class ListContacts extends Component {
                 <p>{contact.handle}</p>
               </div>
               <button
-                onClick={() => this.props.onDeleteContact(contact)}
+                onClick={() => onDeleteContact(contact)}
                 className='contact-remove'>
                   Remove
               </button>
